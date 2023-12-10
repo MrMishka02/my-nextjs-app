f7e50998ae4af832b3a36bcfd035999effa32b1f
Mikheil Gogia <m.gogia@hotmail.com>
2023-12-11 00:31:37 +0400

    added linkedin link in Footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -5,7 +5,14 @@ const Footer = () => {
   return (
     <footer className="flex items-center justify-between bg-slate-400">
       <p className="m-4">2023</p>
-      <p className="m-4">&copy; Mikheil Gogia</p>
+      <p className="m-4">
+        <a
+          href="https://www.linkedin.com/in/mikheil-gogia-848315247/"
+          className="cursor-pointer"
+        >
+          &copy; Mikheil Gogia
+        </a>
+      </p>
       <a
         href="https://github.com/MrMishka02"
         target="_blank"
