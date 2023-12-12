31703b40939ad2d34e95d5b8cc9474b8b6cc6a00
Mikheil Gogia <m.gogia@hotmail.com>
2023-12-12 21:26:14 +0400

    chore: change some styles

--- app/page.tsx
+++ app/page.tsx
@@ -9,57 +9,58 @@ export default function Home() {
   return (
     <main>
       <div
-        className="m-auto flex justify-center items-center h-[40rem] sm:h-[10rem]
-        border-t-2 border-slate-500"
+        className="m-auto flex justify-center items-center h-[40rem]
+        border-t-2 border-slate-500 sm:h-[30rem] sm:flex-col"
       >
         <div
           id="leftDiv"
-          className="w-[60%] h-full flex items-center justify-end"
+          className="w-[60%] h-full flex items-center justify-end
+          sm:flex-col sm:items-center sm:justify-center sm:order-1 sm:w-full"
         >
-          <div className="w-[20rem] mr-7 sm:mr-2 sm:w-[8rem]">
-            <h2 className="text-cyan-600 text-xl sm:text-xs">
+          <div className="w-[20rem] mr-7 sm:mr-0">
+            <h2 className="text-cyan-600 text-xl sm:text-center sm:text-base">
               Hello, My name is
             </h2>
-            <h1 className="text-white text-4xl mt-10 sm:text-xl sm:mt-5">
+            <h1 className="text-white text-4xl mt-10 sm:text-center sm:text-2xl sm:mt-5">
               Mikheil Gogia
             </h1>
-            <h2 className="text-green-500 text-2xl mt-10 sm:text-xs sm:mt-5">
+            <h2 className="text-green-500 text-2xl mt-10 sm:text-center sm:text-base sm:mt-5">
               Full-Stack Developer
             </h2>
           </div>
         </div>
         <div
           id="rightDiv"
-          className="w-[40%] h-full flex items-center justify-center"
+          className="w-[40%] h-full flex items-center justify-center sm:w-full"
         >
-          <div className="flex justify-center sm:w-[6rem]">
+          <div className="flex justify-center sm:w-[8rem]">
             <Image
               src={myPhoto}
               alt="Picture of the author"
-              width={200}
+              width={250}
               style={{ borderRadius: "50%" }}
             />
           </div>
         </div>
       </div>
       <div className="flex-col border-t-2 drop-shadow-lg mt-20">
-        <h2 className="text-lg text-cyan-600 mt-10 text-center">
+        <h2 className="text-lg text-cyan-600 mt-10 text-center sm:text-base">
           Create beautiful, functional and responsive websites.
         </h2>
-        <p className="select-none text-center text-cyan-600 mt-4">
+        <p className="select-none text-center text-cyan-600 mt-4 sm:text-sm">
           For more click:{" "}
-          <Link href={"./projects"} className="underline text-blue-600">
+          <Link href={"./projects"} className="underline text-blue-600 text-sm">
             Projects
           </Link>
         </p>
-        <div className="flex justify-evenly mt-10 mb-40">
-          <div className="w-[28%] pt-8">
+        <div className="flex justify-evenly mt-10 mb-40 sm:flex-col sm:items-center">
+          <div className="w-[28%] pt-8 sm:w-[80%]">
             <Image src={tablet} alt="Project Photo" />
           </div>
-          <div className="w-[38%]">
+          <div className="w-[38%] sm:w-[80%] sm:pt-10">
             <Image src={webPhoto} alt="Project Photo" />
           </div>
-          <div className="w-[32%] pt-7">
+          <div className="w-[32%] pt-7 sm:w-[80%] sm:pt-10">
             <Image src={laptop} alt="Project Photo" />
           </div>
         </div>
