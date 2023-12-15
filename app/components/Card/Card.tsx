fde307167792a47f5ea3334552b856b7e4836289
Mikheil Gogia <m.gogia@hotmail.com>
2023-12-15 21:48:41 +0400

    chore: corrected styles for mobile device

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -6,14 +6,14 @@ interface Props extends StaticImageData {
   path: string | StaticImageData;
   cardH1: string;
   cardText: string;
-  cardLink: string
+  cardLink: string;
 }
 
 function Card({ path, cardH1, cardText, cardLink }: Props) {
   return (
     <div
-      className="w-[24rem] h-[34rem] drop-shadow-md bg-white rounded-lg mt-20 mb-10 pt-2
-      select-none hover:opacity-[0.8] lg:mb-0 sm:w-[20rem] sm:mb-0 sm:mr-0"
+      className="w-[24rem] h-[34rem] drop-shadow-md bg-white rounded-lg ml-2 mr-2 mt-20 mb-10 pt-2
+      select-none hover:opacity-[0.8] lg:mb-0 sm:w-[20rem] sm:mb-0 sm:mr-0 sm:ml-0"
     >
       <div className="w-[96%] h-[50%] m-auto sm:h-[40%]">
         <div className="drop-shadow-md border-2 mb-2 rounded-2xl overflow-hidden">
@@ -27,10 +27,7 @@ function Card({ path, cardH1, cardText, cardLink }: Props) {
           className="w-[30%] h-[8%] ml-[35%] rounded-lg bottom-6 absolute bg-gray-700 text-white
         hover:cursor-pointer hover:bg-gray-500 active:bg-green-700"
         >
-          <Link
-            href={cardLink}
-            target="_blank"
-          >
+          <Link href={cardLink} target="_blank">
             Go to site
           </Link>
         </button>
