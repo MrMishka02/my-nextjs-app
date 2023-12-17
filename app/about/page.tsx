99f3be5e7f892064e18f3626eca0fbe1c1954df4
Mikheil Gogia <m.gogia@hotmail.com>
2023-12-17 15:07:14 +0400

    chore: correct symbol on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -66,7 +66,7 @@ function about() {
             </li>
             <li className="mb-4">
               <span className="font-bold">
-                Bachelor&#789;s academic Degree with Major in Computer
+                Bachelor's academic Degree with Major in Computer
                 Technologies.
               </span>
               <br />
