d36a4a3987700f7896eeac8059d4895ff56ee555
Mikheil Gogia <m.gogia@hotmail.com>
2023-12-20 00:17:08 +0400

    chore: corrected homepage link

--- app/components/Sidebar/Links/Links.tsx
+++ app/components/Sidebar/Links/Links.tsx
@@ -1,7 +1,8 @@
 const Links = () => {
-  const items = ["Home", "Projects", "Contact", "About"];
+  const items = ["Projects", "Contact", "About"];
   return (
     <div className="absolute w-[100%] h-[100%] flex flex-col items-center justify-center gap-[1.25rem]">
+      <a href="/">Homepage</a>
       {items.map((item) => (
         <a href={`${item}`} key={item}>
           {item}
